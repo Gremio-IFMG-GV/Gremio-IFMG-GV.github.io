d3105447cb9ccc473817591c962437db66271981
Lucas <alvespereiralucas133@gmail.com>
2026-09-06 15:08:38 -0300

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -19,3 +19,11 @@ const app = initializeApp(firebaseConfig);
 // Deixa pronto pra outras páginas usarem: o banco de dados (db) e o login (auth)
 export const db = getFirestore(app);
 export const auth = getAuth(app);
+export const firebaseConfig = {
+  apiKey: "AIzaSyA-E3VbgqkelY-eB8jZ65EZJjxDySk46eM",
+  authDomain: "gremio-ifmg.firebaseapp.com",
+  projectId: "gremio-ifmg",
+  storageBucket: "gremio-ifmg.firebasestorage.app",
+  messagingSenderId: "79701581150",
+  appId: "1:79701581150:web:a2ef411b143332f65b43cf"
+};
