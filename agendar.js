e695c51c24cfd0b1a7221a27f4db255a7ad75e12
Lucas <alvespereiralucas133@gmail.com>
2026-09-07 01:23:21 -0300

    Update agendar.js

--- agendar.js
+++ agendar.js
@@ -20,7 +20,7 @@ function mostrarPopup(mensagem, redirecionarParaHome) {
 emailjs.init("Y2p4-JQhyVwsirKI7");
 
 const EMAILS_DESTINO = [
-  "seuemail@exemplo.com"
+  "0117389@academico.ifmg.edu.br"
 ];
 
 const form = document.querySelector("form[data-atividade]");
